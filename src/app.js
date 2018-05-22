8926ec343f1965749ebc6d5f2ff755ad4f58507c
davfry <davecfryer@gmail.com>
2018-05-21 18:40:41 -0600

    commented out dummy data

--- src/app.js
+++ src/app.js
@@ -13,13 +13,13 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'water bill', amount: 1000, createdAt: -1000 }));
-store.dispatch(addExpense({ description: 'gas bill', amount: 3000, createdAt: 0 }));
-store.dispatch(addExpense({ description: 'rent', amount: 50000, createdAt: -21000 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// store.dispatch(addExpense({ description: 'water bill', amount: 1000, createdAt: -1000 }));
+// store.dispatch(addExpense({ description: 'gas bill', amount: 3000, createdAt: 0 }));
+// store.dispatch(addExpense({ description: 'rent', amount: 50000, createdAt: -21000 }));
+// 
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
